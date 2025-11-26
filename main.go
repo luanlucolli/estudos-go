61e8d1e69232fe54acbdfc1aeb38667e024ec6ab
luan <luangstl@gmail.com>
2025-11-25 22:41:10 -0300

    implement square root function and update main to use it

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+func sqrt(x float64) float64 {
+
+	z := 1.0
+
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+
+	return z
+}
+
 func main() {
-
-	fmt.Println("Hello, World!")
-
+	fmt.Println(sqrt(7865867))
 }
