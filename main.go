175ab03f51c82c92db3d97602647c8a2650f778e
luan <luangstl@gmail.com>
2025-11-29 10:56:10 -0300

    refactor sqrt function for improved readability and accuracy

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func sqrt(x float64) float64 {
 
+	fmt.Println("Calculando a raiz quadrada de:", x)
+
+	if x < 0 {
+		return math.NaN()
+	}
+
 	z := 1.0
 
-	for i := 0; i < 10; i++ {
+	for math.Abs(z*z-x) > 1e-18 {
+
 		z -= (z*z - x) / (2 * z)
+		fmt.Println(z)
 	}
 
 	return z
 }
 
 func main() {
-	fmt.Println(sqrt(7865867))
+	fmt.Printf("Função Sqrt personalizada: %f\n", sqrt(64))
+	fmt.Println("Função Sqrt da lib math:", math.Sqrt(64))
 }
