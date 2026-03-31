766fc3e61689340091d6225144f166081776a8f5
luanlucolli <luangstl@gmail.com>
2026-03-31 19:16:44 -0300

    feat: exercicio 1.7 concluido

---
+++ a-linguagem-go/ch1/exercicio-1.7/main.go
@@ -0,0 +1,31 @@
+/*Exercício 1.7: A chamada de função io.Copy(dst, src) lê de src e escreve
+em dst. Use-a no lugar de ioutil.ReadAll para copiar o corpo da
+resposta para os.Stdout sem exigir um buffer grande o suficiente para
+armazenar todo o stream. Não se esqueça de verificar o resultado de erro
+de io.Copy.*/
+
+// Fetch exibe o conteúdo encontrado em cada URL especificada.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
