7d70541c1751775524e71e919d3348392ac7c6b6
luanlucolli <luangstl@gmail.com>
2026-03-31 19:21:47 -0300

    feat: [WIP] exercicio 1.8 em andamento

---
+++ a-linguagem-go/ch1/exercicio-1.8/main.go
@@ -0,0 +1,29 @@
+/*Exercício 1.8: Modifique fetch para que o prefixo http:// seja acrescentado
+a cada URL de argumento, caso esteja faltando. Você pode usar
+strings.HasPrefix.*/
+
+// Fetch exibe o conteúdo encontrado em cada URL especificada.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body) // aproveitei a solução do exercício 1.7
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
