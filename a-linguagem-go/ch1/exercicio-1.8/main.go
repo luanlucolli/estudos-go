18db7cc0b12daa4bcb61b80e80c68c9f8e5778fb
luanlucolli <luangstl@gmail.com>
2026-04-01 07:46:40 -0300

    feat(ch1): conclui exercicio 1.8

--- a-linguagem-go/ch1/exercicio-1.8/main.go
+++ a-linguagem-go/ch1/exercicio-1.8/main.go
@@ -10,10 +10,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	//prefix := "http://"
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
