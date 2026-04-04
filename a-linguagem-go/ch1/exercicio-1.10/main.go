c1c56e76e448d1c11f29471929114646f28e5e38
luanlucolli <luangstl@gmail.com>
2026-04-04 15:17:22 -0300

    feat(ch1/ex1.10): exercicio concluido - add argument validation and file output for fetch results

--- a-linguagem-go/ch1/exercicio-1.10/main.go
+++ a-linguagem-go/ch1/exercicio-1.10/main.go
@@ -10,23 +10,36 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
 
 func main() {
+
+	// valida numero minimo de args
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Uso: go run main.go exec[n] url1 url2...\n")
+		os.Exit(1)
+	}
+
+	//valida arg
+	if matched, _ := regexp.MatchString(`^exec\d+$`, os.Args[1]); !matched {
+		fmt.Fprintf(os.Stderr, "Erro: use o padrão exec[n] (ex: exec1)\n")
+		os.Exit(1)
+	}
+
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := range os.Args[1:] {
+	for _, url := range os.Args[2:] {
 		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
 			url = "http://" + url
 		}
 		go fetch(url, ch) // inicia uma gorrotina
 	}
-	for range os.Args[1:] {
+	for range os.Args[2:] {
 		fmt.Println(<-ch)
 		// recebe do canal ch
 	}
@@ -40,12 +53,28 @@ func fetch(url string, ch chan<- string) {
 		return
 		// envia para o canal ch
 	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close() // evita vazamento de recursos
+
+	defer resp.Body.Close() // evita vazamento de recursos
+
+	filename := strings.ReplaceAll(url, "://", "_")
+	filename = strings.ReplaceAll(filename, "/", "_")
+	filename = strings.ReplaceAll(filename, ":", "")
+
+	file, err := os.Create(os.Args[1] + "_" + filename + ".txt")
+	if err != nil {
+		ch <- fmt.Sprintf("falha ao criar arquivo %s: %v", url, err)
+		return
+	}
+
+	defer file.Close()
+
+	nbytes, err := io.Copy(file, resp.Body)
+
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
