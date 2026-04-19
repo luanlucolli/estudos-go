7a0c022a37a4be33bf25a5540e1672ccc009f0e3
luanlucolli <luangstl@gmail.com>
2026-04-19 00:37:01 -0300

    feat(ch2/ex2.2): ler do stdin quando não houver argumentos

--- a-linguagem-go/ch2/ex2.2/main.go
+++ a-linguagem-go/ch2/ex2.2/main.go
@@ -9,6 +9,7 @@ e operações semelhantes.
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -19,25 +20,49 @@ import (
 )
 
 func main() {
-	//fmt.Println(lenconv.TempFoot.String())
-	//fmt.Println(lenconv.TempMeter.String())
 
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
-			os.Exit(1)
+	args := os.Args[1:]
+
+	//checar se tem args
+	if len(args) > 0 {
+		for _, arg := range args {
+			err := converteArg(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v: erro na conversão: %v\n", arg, err)
+			}
+		}
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Split(bufio.ScanWords)
+		for scanner.Scan() {
+			err := converteArg(scanner.Text())
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v: erro na conversão: %v\n", scanner.Text(), err)
+			}
 		}
 
-		f := tempconv.Fahrenheit(t)
-		c := tempconv.Celsius(t)
-		ft := lenconv.Foot(t)
-		mt := lenconv.Meter(t)
-		p := weightconv.Pound(t)
-		kg := weightconv.Kilogram(t)
-		//peso em libras
-		//peso em quilogramas
-		fmt.Printf("%s = %s, %s = %s\n%s = %s, %s = %s\n%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c), ft, lenconv.FToM(ft), mt, lenconv.MToF(mt), kg, weightconv.KToP(kg), p, weightconv.PToK(p))
-
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "erro na leitura: %v\n", err)
+			os.Exit(1)
+		}
 	}
+}
+
+func converteArg(s string) error {
+	v, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return fmt.Errorf("número inválido '%s': %v", s, err)
+	}
+	f := tempconv.Fahrenheit(v)
+	c := tempconv.Celsius(v)
+	ft := lenconv.Foot(v)
+	mt := lenconv.Meter(v)
+	p := weightconv.Pound(v)
+	kg := weightconv.Kilogram(v)
+
+	fmt.Printf("--- Conversões para %g ---\n", v)
+	fmt.Printf("Temperatura: %s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	fmt.Printf("Comprimento: %s = %s, %s = %s\n", ft, lenconv.FToM(ft), mt, lenconv.MToF(mt))
+	fmt.Printf("Peso:        %s = %s, %s = %s\n\n", kg, weightconv.KToP(kg), p, weightconv.PToK(p))
+	return nil
 }
