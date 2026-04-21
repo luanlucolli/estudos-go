62ac07d186f2e1537a5e3ec5797338fd3acd58d6
luanlucolli <luangstl@gmail.com>
2026-04-21 12:48:04 -0300

    feat(ch2/ex2.5): concluir exercicio ch2/ex2.5

--- gopl/ch2/ex2.5/main.go
+++ gopl/ch2/ex2.5/main.go
@@ -1,1 +1,24 @@
+/*
+Exercício 2.5: A expressão x&(x-1) limpa o bit diferente de zero mais à
+direita de x. Escreva uma versão de PopCount que conte bits usando esse
+fato e avalie seu desempenho.
+*/
 package main
+
+import "fmt"
+
+func PopCount(x uint64) int {
+	count := 0
+
+	for x != 0 {
+		x = x & (x - 1)
+		count++
+	}
+
+	return count
+}
+
+func main() {
+	var value uint64 = 22
+	fmt.Println(PopCount(value))
+}
