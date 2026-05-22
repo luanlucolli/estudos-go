8491a2e19b022896167041481263450f3be04202
luanlucolli <luangstl@gmail.com>
2026-05-22 20:08:18 -0300

    feat(ch3/ex3.4): implementa seletor de formulas e refatora ciclo de vida do renderizador com structs

--- gopl/ch3/ex3.4/main.go
+++ gopl/ch3/ex3.4/main.go
@@ -18,7 +18,19 @@ import (
 	"strconv"
 )
 
+type RenderContext struct {
+	width, height      float64
+	xyscale, zscale    float64
+	sin30, cos30       float64
+	cells              float64
+	xyrange            float64
+	angle              float64
+	maxColor, minColor string
+	surfacaceFunction  surfaceFunc
+}
+
 // molde da função passada como argumento na função corner
+type surfaceFunc func(x, y float64) float64
 
 func main() {
 
@@ -32,25 +44,72 @@ func main() {
 
 func surface3D(w http.ResponseWriter, r *http.Request) {
 
-	// verifica se o path é /favicon.ico e retorna caso seja
-	if r.URL.Path == "/favicon.ico" {
-		return
-	}
+	// define a forma da superficie 3d
+	var surfaceFunction surfaceFunc = sombrero
 
-	const (
-		cells   = 100.0 // número de células da grade
-		xyrange = 30.0  // intervalos dos eixos (-xyrange..+xyrange)
-	)
-
+	// cria maps com os valores aceitos no query params da url
 	surfaceColors := map[string]string{
 		"maxColor": "#ff0000",
 		"minColor": "#0000ff",
 	}
-
 	surfaceValues := map[string]float64{
 		"width":  600,
 		"height": 320,
 	}
+
+	// chama a função que extrai os valores dos query params e atualiza nos maps
+	processQueryParams(surfaceValues, surfaceColors, &surfaceFunction, r)
+
+	// monta a struct e define valores que não dependem de operações com outros valores da mesma struct
+	ctx := &RenderContext{
+		width:    surfaceValues["width"],
+		height:   surfaceValues["height"],
+		maxColor: surfaceColors["maxColor"],
+		minColor: surfaceColors["minColor"],
+		cells:    100.0,
+		xyrange:  30.0,
+		angle:    math.Pi / 6,
+	}
+
+	// termina de definir valores da sruct
+	ctx.xyscale = ctx.width / 2 / ctx.xyrange
+	ctx.zscale = ctx.height * 0.4
+	ctx.sin30 = math.Sin(ctx.angle)
+	ctx.cos30 = math.Cos(ctx.angle)
+
+	// seta o header para image/svg+xml
+	w.Header().Set("Content-Type", "image/svg+xml")
+
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; strokewidth: 0.7' "+
+		"width='%g' height='%g'>", ctx.width, ctx.height)
+	for i := 0; i < int(ctx.cells); i++ {
+		for j := 0; j < int(ctx.cells); j++ {
+			ax, ay, zA, okA := ctx.corner(i+1, j, surfaceFunction)
+			bx, by, zB, okB := ctx.corner(i, j, surfaceFunction)
+			cx, cy, zC, okC := ctx.corner(i, j+1, surfaceFunction)
+			dx, dy, zD, okD := ctx.corner(i+1, j+1, surfaceFunction)
+
+			zAverage := (zA + zB + zC + zD) / 4.0
+
+			if !okA || !okB || !okC || !okD {
+				continue
+			}
+
+			color := polygonColor(zAverage, ctx.maxColor, ctx.minColor)
+
+			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
+		}
+	}
+
+	fmt.Fprintf(w, "</svg>")
+
+}
+
+// extrai os query params e atualiza os maps se houver valor nas keys
+// não retorna nada, atualiza o map diretamente
+func processQueryParams(surfaceValues map[string]float64, surfaceColors map[string]string, surfaceFunction *surfaceFunc, r *http.Request) {
 
 	query := r.URL.Query()
 
@@ -84,82 +143,34 @@ func surface3D(w http.ResponseWriter, r *http.Request) {
 
 	}
 
-	width := surfaceValues["width"]
-	height := surfaceValues["height"]
-	maxColor := surfaceColors["maxColor"]
-	minColor := surfaceColors["minColor"]
-	xyscale := width / 2 / xyrange // pixels por unidade x ou y
-	zscale := height * 0.4         // pixels por unidade z
-	angle := math.Pi / 6
-	sin30 := math.Sin(angle)
-	cos30 := math.Cos(angle)
+	tipo := query.Get("type")
 
-	type surfaceFunc func(x, y float64) float64
-
-	corner := func(i, j int, f surfaceFunc) (float64, float64, float64, bool) {
-		// Encontra o ponto (x,y) no canto da célula (i,j)
-		x := xyrange * (float64(i)/cells - 0.5)
-		y := xyrange * (float64(j)/cells - 0.5)
-		// Calcula a altura z da superfície
-		z := f(x, y)
-		if math.IsInf(z, 0) || math.IsNaN(z) {
-			return 0, 0, 0, false
-		}
-		// Faz uma projeção isométrica de (x,y,z) sobre (sx,sy) do canvas SVG 2D
-		sx := width/2 + (x-y)*cos30*xyscale
-		sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-		return sx, sy, z, true
+	switch tipo {
+	case "sombrero":
+		*surfaceFunction = sombrero
+	case "eggbox":
+		*surfaceFunction = eggBox
+	case "saddle":
+		*surfaceFunction = saddle
+	case "moguls":
+		*surfaceFunction = moguls
 	}
-
-	// query params deve aceitar altura, largura e cor
-
-	w.Header().Set("Content-Type", "image/svg+xml")
-
-	funcaoEscolhida := eggBox
-
-	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; strokewidth: 0.7' "+
-		"width='%g' height='%g'>", width, height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay, zA, okA := corner(i+1, j, funcaoEscolhida)
-			bx, by, zB, okB := corner(i, j, funcaoEscolhida)
-			cx, cy, zC, okC := corner(i, j+1, funcaoEscolhida)
-			dx, dy, zD, okD := corner(i+1, j+1, funcaoEscolhida)
-
-			zAverage := (zA + zB + zC + zD) / 4.0
-
-			if !okA || !okB || !okC || !okD {
-				continue
-			}
-
-			color := polygonColor(zAverage, maxColor, minColor)
-
-			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy, color)
-		}
-	}
-
-	fmt.Fprintf(w, "</svg>")
 
 }
 
-/*
-	func corner(i, j int, f surfaceFunc) (float64, float64, float64, bool) {
-		// Encontra o ponto (x,y) no canto da célula (i,j)
-		x := xyrange * (float64(i)/cells - 0.5)
-		y := xyrange * (float64(j)/cells - 0.5)
-		// Calcula a altura z da superfície
-		z := f(x, y)
-		if math.IsInf(z, 0) || math.IsNaN(z) {
-			return 0, 0, 0, false
-		}
-		// Faz uma projeção isométrica de (x,y,z) sobre (sx,sy) do canvas SVG 2D
-		sx := width/2 + (x-y)*cos30*xyscale
-		sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-		return sx, sy, z, true
+func (ctx *RenderContext) corner(i, j int, f surfaceFunc) (float64, float64, float64, bool) { // Encontra o ponto (x,y) no canto da célula (i,j)
+	x := ctx.xyrange * (float64(i)/ctx.cells - 0.5)
+	y := ctx.xyrange * (float64(j)/ctx.cells - 0.5)
+	// Calcula a altura z da superfície
+	z := f(x, y)
+	if math.IsInf(z, 0) || math.IsNaN(z) {
+		return 0, 0, 0, false
 	}
-*/
+	// Faz uma projeção isométrica de (x,y,z) sobre (sx,sy) do canvas SVG 2D
+	sx := ctx.width/2 + (x-y)*ctx.cos30*ctx.xyscale
+	sy := ctx.height/2 + (x+y)*ctx.sin30*ctx.xyscale - z*ctx.zscale
+	return sx, sy, z, true
+}
 
 func sombrero(x, y float64) float64 {
 	r := math.Hypot(x, y) // distância de (0,0)
